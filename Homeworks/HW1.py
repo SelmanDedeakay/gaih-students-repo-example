31e3ca79d4b1537e1b022b3ee984833f202831f9
Selman Dedeakayoğulları <selmandedeakayogullari@gmail.com>
2021-03-24 03:32:54 +0300

    HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -2,8 +2,7 @@
 
 #1) How would you define Machine Learning?
 
-# In my opinion, Machine Learning is our way of solving the problems which we encountered with some of algorithms that our machine can understand.
-
+# In my opinion, Machine Learning is the way of solving the problems which we encountered with some of algorithms that our machine can understand.
 
 #2) What are the differences between Supervised and Unsupervised Learning? Specify example 3 algorithms for each of these.
 
@@ -18,10 +17,14 @@
 # Test dataset is the data we are using to test our final machine learning model. Validation dataset is the data we are using to determine which model is the best for our goal and we are using it while tuning our hyperparameters.
 # If we don't use them our model may have high incorrect prediction rate.
 
-#5) How you can explore continous and discrete variables?
+#5) How you can explore continuous and discrete variables?
 
-# By 
+# Continuous variables are measurable. We can show them with line graphs
+# Discrete variables are countable. We can show them with scatter graphs
 
 #6) Analyse the plot given below. (What is the plot and variable type, check the distribution and make comment about how you can preprocess it.)
 
-# Our plot is histogram. Our variable type is continous.
+# Our plot is histogram. Our variable type is continuous.
+# There is some zeros in graph and there may be some missing values in the dataset. 
+# We should assign the missing values to our median.
+# There is some anomalies in between 0 and 1. Our data may be an imbalanced data , there may be some duplicated values in dataset or some outliers.
